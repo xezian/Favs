faa2a8e171fd1c77a71bfcead78d489e461a9c35
Jason A Leo <30601823+xezian@users.noreply.github.com>
2022-01-07 22:18:19 -0500

    This proves frontend connected to backend

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import logo from './logo.svg';
+import axios, { AxiosResponse } from 'axios';
 import './App.css';
 
 function App() {
+  const [bird, setBird] = useState("")
+  
+  const fetchOurData = async () => {
+    const response = await axios.get("http://localhost:1000/") as AxiosResponse<string, string>
+    setBird(response.data)
+  }
+
+  useEffect(() => {
+    fetchOurData()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {bird}
         </p>
         <a
           className="App-link"
